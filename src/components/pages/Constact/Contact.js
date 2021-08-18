74ae9cfe7aec86c84f274efb05cd5d7e64d021a1
Izzy <browncans@gmail.com>
2021-08-17 22:37:52 -0700

    form implemented

--- src/components/pages/Constact/Contact.js
+++ src/components/pages/Constact/Contact.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React from "react";
+import "./Contact.css";
+
+const style = {
+  float: "right",
+};
 
 export default function Contact() {
   return (
     <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <div className="container">
+        <div className="row">
+          <h1>contact us</h1>
+        </div>
+        <div className="row"></div>
+        <div className="row input-container">
+          <div className="col-xs-12">
+            <div className="styled-input wide">
+              <input type="text" required />
+              <label>Name</label>
+            </div>
+          </div>
+          <div className="col-md-6 col-sm-12">
+            <div className="styled-input">
+              <input type="text" required />
+              <label>Email</label>
+            </div>
+          </div>
+          <div className="col-md-6 col-sm-12">
+            <div className="styled-input" style={style}>
+              <input type="text" required />
+              <label>Phone Number</label>
+            </div>
+          </div>
+          <div className="col-xs-12">
+            <div className="styled-input wide">
+              <textarea required></textarea>
+              <label>Message</label>
+            </div>
+          </div>
+          <div className="col-xs-12">
+            <div className="btn-lrg submit-btn">Send Message</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
