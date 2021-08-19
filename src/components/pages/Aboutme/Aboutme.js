3bbfbe28cc20172b821ef3a40d2ee546de35f7f1
Izzy <browncans@gmail.com>
2021-08-18 23:26:37 -0700

    my lovely pic

--- src/components/pages/Aboutme/Aboutme.js
+++ src/components/pages/Aboutme/Aboutme.js
@@ -1,20 +1,28 @@
 import React from "react";
 import "./Aboutme.css";
+import avatar from "../../../assets/imgs/avatar.png";
 
 export default function Aboutme() {
   return (
     <div className="dib">
       <h1>About Me</h1>
-      <p className="fix">
-        I am an aspiring web developer based in North County San Diego. A few of
-        my favorite things are rock climbing, creating art, and spending time
-        with friends and family. I went to school up in San Jose State
-        University and graduated with degree in Spanish. I spent some time after
-        graduation working at a restaurant. It has taken me a while but I have
-        decided to take a deep dive into a totally different career path. I hope
-        to learn lots and be able to apply the concepts learned during class in
-        the real world.
-      </p>
+      <div className="square">
+        <p className="fix">
+          <span>
+            {" "}
+            <img src={avatar} alt="my avatar" />
+          </span>
+          <br />
+          <br />I am an aspiring web developer based in North County San Diego.
+          A few of my favorite things are rock climbing, creating art, and
+          spending time with friends and family. I went to school up in San Jose
+          State University and graduated with degree in Spanish. I spent some
+          time after graduation working at a restaurant. It has taken me a while
+          but I have decided to take a deep dive into a totally different career
+          path. I hope to learn lots and be able to apply the concepts learned
+          during class in the real world.
+        </p>
+      </div>
     </div>
   );
 }
